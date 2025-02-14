7fc17cfafb013a63ec4ec43dbe9d3fe667ae6fa9
Fabio Sánchez <fabiosanchez@MacBook-Pro-de-Fabio.local>
2025-02-14 14:44:49 -0500

    Respaldo el código con la funcionalidad de votar agragar

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,7 +1,17 @@
 const { io } = require('../index'); // Importa la propiedad io correctamente
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon jovi'));
+bands.addBand(new Band('Metallica'));
+console.log('Init server');
+console.log(bands); 
 
 io.on('connection', (client) => {
-    console.log('Client connected', client.id);
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', (message) => {
         console.log('Client disconnected', message);
@@ -15,5 +25,21 @@ io.on('connection', (client) => {
     client.on('emitir-mensaje', (payload) => {
         console.log('Message received', payload);
         io.emit('nuevo-mensaje', payload);
+    });
+
+    client.on('register-band', (payload) => {
+        const newBand = new Band(payload.name);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('vote-band', (id) => {
+        bands.voteBand(id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (id) => {
+        bands.deleteBand(id);
+        io.emit('active-bands', bands.getBands());
     });
 });
